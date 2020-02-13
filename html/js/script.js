98d9c9132263a2ce30a2ed5cdb31359b9cea15fd
Desenvolvimento T66641 <t66641@SENAI-LAURO.LOCAL>
2020-02-12 21:28:24 -0300

    Alteracao da tela com js

---
+++ html/js/script.js
@@ -0,0 +1,60 @@
+//alert()
+//console.log
+//console.warn
+//console.error
+
+{/* <div id="form_pokesystem" >
+<form action="pokesystem.php" method="get">
+<div id="titulo">
+    <h3>Pokesystem</h3>
+</div>
+
+<div id="nome">
+    <label>Nome:</label>
+    <input type="text" id="nome_user" placeholder="Digite o nome do pokemon"/>                
+</div>    
+
+<div id="tipo">
+    <label>Tipo:</label>
+    <select id="tipo">
+        <option value="">--Selecione</option>
+        <option value="eletrico" id="tipo_eletrico">Elétrico</option>
+        <option value="fogo" id="tipo_fogo">Fogo</option>
+        <option value="agua" id="tipo_agua">Água</option>
+        <option value="terra" id="tipo_terra">Terra</option>
+        <option value="gelo" id="tipo_gelo">Gelo</option>
+        <option value="psiquico" id="tipo_psiquico">Psíquico</option>
+        <option value="planta" id="tipo_planta">Planta</option>
+    </select>
+</div>
+
+<div id="poder">
+    <div id="ataque">
+        <label>Ataque:</label>
+        <input type="number" id="ataque" placeholder="0-100"/>
+        <label>Evoluido?</label>
+    </div>
+    
+    <div id="defesa">
+        <label>Defesa:</label>
+        <input type="number" id="defesa" placeholder="0-100"/>
+        <input type="radio" name="evoluido" checked>Sim
+        <input type="radio" name="evoluido">Não                    
+    </div>
+</div>
+
+
+
+<div id="descricao">
+    <label>Descrição:</label>
+    <textarea id="descricao_pokemon" placeholder="Quais as características do seu pokemon ?" cols="30" rows="10"></textarea>
+</div>
+
+<div id="botao">
+    <button id="botao_salvar">Salvar</button>
+</div>
+</form>
+</div>     */}
+
+var chef = document.getElementById(app)
+
